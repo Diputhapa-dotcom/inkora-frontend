688bd730dece76cc3ecb0075239cc5583a5bb34d
Diputhapa-dotcom <magardeepu53@gmail.com>
2026-08-07 00:05:26 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Register from './auth/Register'
+
+const App = () => {
+  return (
+       <BrowserRouter>
+       <Routes>
+        <Route path='/register' element={<Register/> } />
+       </Routes>
+       </BrowserRouter>
+
+  )
+}
+
+export default App
---
+++ src/auth/Register.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Auth from './component/Auth'
+
+const Register = () => {
+
+
+  return (
+    <Auth type={register}/>
+  )
+}
+
+export default Register
