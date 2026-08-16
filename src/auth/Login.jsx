5423a65c5b5e87e7c79dd246e17a49029a5284cc
Diputhapa-dotcom <magardeepu53@gmail.com>
2026-08-17 00:21:05 +0545

    debugging

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -2,19 +2,20 @@ import React from 'react'
 import Auth from './component/Auth'
 import axios from 'axios'
 import { urlBase } from '../../config'
+import { useDispatch, useSelector } from 'react-redux'
+import { user } from '../store/userSlice'
 
 const Login = () => {
+  const {status}= useSelector((state)=>state.auth)
+  console.log(status)
+  const dipatch = useDispatch()
     const handleLogin =async  (data) =>{
-     const response = await axios.post(`${urlBase}/login`,data,{
-        headers : {
-          "Authorization" : localStorage.getItem('token')
-        }
-       })
-       if(response.status === 201){
-        console.log("logged in successfully")
-       }
+      console.log(data)
+       dispatch( user( data ) )
 
-    }
+
+
+  }
   return (
 <Auth type="Login" onsub = {handleLogin}/>  )
 }
